ae6d3827d848d345ea52887f2d092ee7329a4dee
gyeongseok <gseok.seo@gmail.com>
2018-06-08 20:47:23 +0900

    Add init code

---
+++ src/test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../styles/styles.scss';
+import staticImgSrc from '../assets/images/love.jpg';
+
+class App extends React.Component {
+    render() {
+        const { port } = window.location;
+        const hardURL = (port === '4000') ? './assets/images/love.jpg' : '../../love.jpg';
+        console.log('>>> URL >', hardURL)
+        // ref: https://medium.com/a-beginners-guide-for-webpack-2/handling-images-e1a2a2c28f8d
+        // ref: https://github.com/reactjs/react-rails/issues/211
+
+        return (
+            <div>
+                <div>Image Test...!!, Static image Source, src changed base64 at webpack (url-loader)</div>
+                <img src={staticImgSrc}/>
+                <br/>
+                <br/>
+                <div>Image Test...!!, src for build(deploy) real image file must exists in relate path </div>
+                <div>in this case, webpack build's copy logic</div>
+                <img src={hardURL} />
+                <br />
+                <br />
+                <div>Test using scss~!~!</div>
+                <div className='myImageTest'></div>
+            </div>
+        );
+    }
+}
+
+const appRoot = document.getElementById('root');
+ReactDOM.render(<App />, appRoot);
---
+++ webpack.config.prod.js
@@ -0,0 +1,69 @@
+// dev config
+const path = require('path');
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const outPath = path.resolve(__dirname, 'out/a/b/c/d');
+
+module.exports = {
+    devtool: 'inline-source-map',
+    entry: path.resolve(__dirname, 'src/test.js'),
+
+    output: {
+        path: outPath,
+        filename: 'test.js'
+    },
+
+    plugins: [
+        // webpack-dev-server enhancement plugins
+        new webpack.HotModuleReplacementPlugin(),
+        new CleanWebpackPlugin([
+            path.resolve(__dirname, 'out')
+        ]),
+        new CopyWebpackPlugin([{
+            from: path.resolve(__dirname, 'src/index.html'),
+            to: path.resolve(outPath, 'index.html')
+        }, {
+            // image copy
+            from: path.resolve(__dirname, 'assets/images/love.jpg'),
+            to: path.resolve(outPath, '../../love.jpg')
+        }]),
+        new ExtractTextPlugin("styles.css"),
+    ],
+
+    devServer: {
+        hot: true,
+        port: 4000,
+        contentBase: path.resolve(__dirname, 'out')
+    },
+
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: [/node_modules/],
+            use: [{
+                loader: 'babel-loader',
+                options: {
+                    presets: ['es2015', 'react']
+                }
+            }]
+        }, {
+            test: /\.(css|scss)$/,
+            exclude: /node_modules/,
+            use: ExtractTextPlugin.extract({
+                use: ['css-loader', 'sass-loader'],
+                fallback: 'style-loader'
+            })
+        }, {
+            test: /\.(gif|png|jpe?g|svg)$/i,
+            use: [{
+                loader: 'url-loader',
+                options: {
+                    limit: 8192
+                }
+            }]
+        }]
+    }
+};
